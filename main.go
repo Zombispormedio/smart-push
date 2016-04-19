7af8bde28fa0ed297cc3449754553eb3eeeca31e
Xavier Serrano <zombispormedio007@gmail.com>
2016-04-19 17:26:28 +0200

    push

--- main.go
+++ main.go
@@ -1,20 +1,45 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
+	"net/http"
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	"github.com/labstack/echo/middleware"
+	"golang.org/x/net/websocket"
 )
 
 func main() {
-    http.HandleFunc("/", hello)
-    fmt.Println("listening...")
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-    if err != nil {
-      panic(err)
-    }
-}
 
-func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "hello, world")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "5065"
+	}
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/ws", standard.WrapHandler(websocket.Handler(func(ws *websocket.Conn) {
+		for {
+			websocket.Message.Send(ws, "Hello, Client!")
+			msg := ""
+			log.WithFields(log.Fields{
+				"animal": "walrus",
+			}).Info("A walrus appears")
+			websocket.Message.Receive(ws, &msg)
+			println(msg)
+		}
+	})))
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!\n")
+	})
+
+	e.Run(standard.New(":" + port))
+
 }
