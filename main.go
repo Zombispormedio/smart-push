09dc0d76b0cc06a7ec8566af285acbed58833898
Xavier Serrano <zombispormedio007@gmail.com>
2016-04-19 15:57:24 +0200

    first server websocket

--- main.go
+++ main.go
@@ -1,34 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
-	"github.com/Zombispormedio/smartrealtime/routes"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	"github.com/labstack/echo/middleware"
+	"golang.org/x/net/websocket"
 )
 
 func main() {
-	fmt.Println(routes.BuildHello())
+
 	port := os.Getenv("PORT")
-    
+
 	if port == "" {
-		port="5060"
+		port = "5065"
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	
+	e := echo.New()
 
-	router.GET("/", func(c *gin.Context) {
-        
-        var msg struct{
-            Message string
-        }
-        
-        msg.Message="Hello World"
-		c.JSON(200, msg)
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/ws", standard.WrapHandler(websocket.Handler(func(ws *websocket.Conn) {
+		for {
+			websocket.Message.Send(ws, "Hello, Client!")
+			msg := ""
+			websocket.Message.Receive(ws, &msg)
+			println(msg)
+		}
+	})))
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!\n")
 	})
 
-	router.Run(":" + port)
+	e.Run(standard.New(":" + port))
+
 }
