27d6b3b74ec84d9a506d17e2ab73b4fba0938801
Xavier Serrano <zombispormedio007@gmail.com>
2016-04-20 11:55:30 +0200

    test

--- bolt_test.go
+++ bolt_test.go
@@ -1,15 +1,26 @@
-package test
+package verbose
 import (
-    "log"
     "testing"
     "github.com/boltdb/bolt"
 )
 
 
-func TestMain(m *testing.M){
-     db, err := bolt.Open("my.db", 0600, nil)
+
+func TestMain(m *testing.T){
+    
+     db, err := bolt.Open("main.db", 0600, nil)
     if err != nil {
-        log.Fatal(err)
+       m.Fatal(err);
     }
     defer db.Close()
+    
+    db.Update(func(tx *bolt.Tx) error{
+         b, _ :=tx.CreateBucketIfNotExists([]byte("Config"))
+      
+         
+        return b.Put([]byte("identifier"), []byte("n6NH976vNOHlWQwGH83uvXS9bTsrUtYb"))
+    })
+    
+  m.Log("hello")
+    
 }
