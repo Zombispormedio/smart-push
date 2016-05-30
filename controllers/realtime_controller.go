0e9e1c9829715bec2d242b257a7a0ca6fe09109e
Xavier Serrano <zombispormedio007@gmail.com>
2016-05-30 11:50:07 +0200

    unix

--- controllers/realtime_controller.go
+++ controllers/realtime_controller.go
@@ -2,9 +2,10 @@ package controllers
 
 import (
 	"os"
-
+"time"
 	"github.com/Zombispormedio/smart-push/lib/redis"
 	"github.com/Zombispormedio/smart-push/lib/response"
+	
 )
 
 func GetRealtimeData(sensor *response.RealTimeData) error {
@@ -22,8 +23,10 @@ func GetRealtimeData(sensor *response.RealTimeData) error {
 		Error = SensorDataError
 
 	} else {
+		var date=dataMap["date"]
+		unixDate, _ := strconv.ParseInt(date, 10, 64)
 		sensor.Value = dataMap["value"]
-		sensor.TimeStamp = dataMap["date"]
+		sensor.TimeStamp = time.Unix(unixDate, 0)
 	}
 
 	return Error
